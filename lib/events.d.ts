197f9851890c185307dff438ab5cc55b6449c41b
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2016-10-15 14:53:39 +0200

    off now consider ctx, when removing listeners

--- lib/events.d.ts
+++ lib/events.d.ts
@@ -1,53 +1,333 @@
+/**
+ *
+ *
+ * @export
+ * @class EventEmitterError
+ * @extends {Error}
+ */
 export declare class EventEmitterError extends Error {
     message: string;
     method: string;
     klass: any;
     ctx: any;
-    constructor(message?: string, method?: string, klass?: any, ctx?: any);
+    /**
+     * Creates an instance of EventEmitterError.
+     *
+     * @param {string} [message]
+     * @param {string} [method]
+     * @param {*} [klass]
+     * @param {*} [ctx]
+     *
+     * @memberOf EventEmitterError
+     */
+    constructor(message: string, method?: string, klass?: any, ctx?: any);
+    /**
+     *
+     *
+     * @returns
+     *
+     * @memberOf EventEmitterError
+     */
     toString(): string;
 }
+/**
+ *
+ *
+ * @export
+ * @interface EventHandler
+ */
 export interface EventHandler {
     (...args: any[]): any;
 }
+/**
+ *
+ *
+ * @export
+ * @interface Events
+ */
 export interface Events {
+    /**
+     *
+     *
+     * @type {string}
+     * @memberOf Events
+     */
     name: string;
+    /**
+     *
+     *
+     * @type {boolean}
+     * @memberOf Events
+     */
     once: boolean;
+    /**
+     *
+     *
+     * @type {EventHandler}
+     * @memberOf Events
+     */
     handler: EventHandler;
+    /**
+     *
+     *
+     * @type {*}
+     * @memberOf Events
+     */
     ctx?: any;
 }
+/**
+ *
+ *
+ * @export
+ * @interface IEventEmitter
+ */
 export interface IEventEmitter {
-    listeners: {
+    /**
+     *
+     *
+     * @type {{ [key: string]: Events[] }}
+     * @memberOf IEventEmitter
+     */
+    listeners?: {
         [key: string]: Events[];
     };
+    /**
+     *
+     *
+     * @type {string}
+     * @memberOf IEventEmitter
+     */
     listenId?: string;
+    /**
+     *
+     *
+     * @param {string} event
+     * @param {EventHandler} fn
+     * @param {*} [ctx]
+     * @returns {*}
+     *
+     * @memberOf IEventEmitter
+     */
     on(event: string, fn: EventHandler, ctx?: any): any;
+    /**
+     *
+     *
+     * @param {string} event
+     * @param {EventHandler} fn
+     * @param {*} [ctx]
+     * @returns {*}
+     *
+     * @memberOf IEventEmitter
+     */
     once(event: string, fn: EventHandler, ctx?: any): any;
+    /**
+     *
+     *
+     * @param {string} event
+     * @param {EventHandler} [fn]
+     * @param {*} [ctx]
+     * @returns {*}
+     *
+     * @memberOf IEventEmitter
+     */
     off(event: string, fn?: EventHandler, ctx?: any): any;
+    /**
+     *
+     *
+     * @param {string} event
+     * @param {...any[]} args
+     * @returns {*}
+     *
+     * @memberOf IEventEmitter
+     */
     trigger(event: string, ...args: any[]): any;
 }
+/**
+ *
+ *
+ * @export
+ * @interface Destroyable
+ */
 export interface Destroyable {
+    /**
+     *
+     *
+     *
+     * @memberOf Destroyable
+     */
     destroy(): any;
 }
+/**
+ *
+ *
+ * @export
+ * @param {Events[]} fn
+ * @param {any[]} [args=[]]
+ * @returns
+ */
 export declare function callFunc(fn: Events[], args?: any[]): void;
+/**
+ *
+ *
+ * @export
+ * @param {*} a
+ * @returns {a is Function}
+ */
 export declare function isFunction(a: any): a is Function;
+/**
+ *
+ *
+ * @export
+ * @param {*} a
+ * @returns {a is EventEmitter}
+ */
 export declare function isEventEmitter(a: any): a is EventEmitter;
+/**
+ *
+ *
+ * @export
+ * @class EventEmitter
+ * @implements {IEventEmitter}
+ * @implements {Destroyable}
+ */
 export declare class EventEmitter implements IEventEmitter, Destroyable {
+    /**
+     *
+     *
+     * @static
+     * @type {boolean}
+     * @memberOf EventEmitter
+     */
     static throwOnError: boolean;
+    /**
+     *
+     *
+     * @static
+     *
+     * @memberOf EventEmitter
+     */
     static debugCallback: (className: string, name: string, event: string, args: any[], listeners: Events[]) => void;
+    /**
+     *
+     *
+     * @static
+     *
+     * @memberOf EventEmitter
+     */
     static executeListenerFunction: (func: Events[], args?: any[]) => void;
+    /**
+     *
+     *
+     * @type {string}
+     * @memberOf EventEmitter
+     */
     listenId: string;
     private _listeners;
     private _listeningTo;
-    listeners: {
+    /**
+     *
+     *
+     * @readonly
+     * @type {{ [key: string]: Events[] }}
+     * @memberOf EventEmitter
+     */
+    readonly listeners: {
         [key: string]: Events[];
     };
+    /**
+     *
+     *
+     * @param {string} event
+     * @param {EventHandler} fn
+     * @param {*} [ctx]
+     * @param {boolean} [once=false]
+     * @returns {*}
+     *
+     * @memberOf EventEmitter
+     */
     on(event: string, fn: EventHandler, ctx?: any, once?: boolean): any;
+    /**
+     *
+     *
+     * @param {string} event
+     * @param {EventHandler} fn
+     * @param {*} [ctx]
+     * @returns {*}
+     *
+     * @memberOf EventEmitter
+     */
     once(event: string, fn: EventHandler, ctx?: any): any;
-    off(eventName?: string, fn?: EventHandler): any;
+    /**
+     *
+     *
+     * @param {string} [eventName]
+     * @param {EventHandler} [fn]
+     * @param {*} [ctx]
+     * @returns {*}
+     *
+     * @memberOf EventEmitter
+     */
+    off(eventName?: string, fn?: EventHandler, ctx?: any): any;
+    /**
+     *
+     *
+     * @param {string} eventName
+     * @param {...any[]} args
+     * @returns {*}
+     *
+     * @memberOf EventEmitter
+     */
     trigger(eventName: string, ...args: any[]): any;
+    /**
+     *
+     *
+     * @param {Events[]} func
+     * @param {any[]} [args]
+     *
+     * @memberOf EventEmitter
+     */
     _executeListener(func: Events[], args?: any[]): void;
+    /**
+     *
+     *
+     * @param {IEventEmitter} obj
+     * @param {string} event
+     * @param {EventHandler} fn
+     * @param {*} [ctx]
+     * @param {boolean} [once=false]
+     * @returns {*}
+     *
+     * @memberOf EventEmitter
+     */
     listenTo(obj: IEventEmitter, event: string, fn: EventHandler, ctx?: any, once?: boolean): any;
+    /**
+     *
+     *
+     * @param {IEventEmitter} obj
+     * @param {string} event
+     * @param {EventHandler} fn
+     * @param {*} [ctx]
+     * @returns {*}
+     *
+     * @memberOf EventEmitter
+     */
     listenToOnce(obj: IEventEmitter, event: string, fn: EventHandler, ctx?: any): any;
+    /**
+     *
+     *
+     * @param {IEventEmitter} [obj]
+     * @param {string} [event]
+     * @param {EventHandler} [callback]
+     * @returns
+     *
+     * @memberOf EventEmitter
+     */
     stopListening(obj?: IEventEmitter, event?: string, callback?: EventHandler): this;
+    /**
+     *
+     *
+     *
+     * @memberOf EventEmitter
+     */
     destroy(): void;
 }
--- src/events.ts
+++ src/events.ts
@@ -1,116 +1,343 @@
 
 var idCounter = 0
-function getID (): string {
+function getID(): string {
   return "" + (++idCounter)
 }
 
+/**
+ * 
+ * 
+ * @export
+ * @class EventEmitterError
+ * @extends {Error}
+ */
 export class EventEmitterError extends Error {
-	constructor(public message?: string, public method?:string, public klass?: any, public ctx?:any) {
-		super(message);
-	}
+  /**
+   * Creates an instance of EventEmitterError.
+   * 
+   * @param {string} [message]
+   * @param {string} [method]
+   * @param {*} [klass]
+   * @param {*} [ctx]
+   * 
+   * @memberOf EventEmitterError
+   */
+  constructor(public message: string, public method?: string, public klass?: any, public ctx?: any) {
+    super(message);
+  }
 
-	toString() {
-		let prefix = "EventEmitterError";
-		if (this.method && this.method != "") {
-			prefix = `EventEmitter#${this.method}`;
-		}
-		return `${prefix}: ${this.message}`;
-	}
+  /**
+   * 
+   * 
+   * @returns
+   * 
+   * @memberOf EventEmitterError
+   */
+  toString() {
+    let prefix = "EventEmitterError";
+    if (this.method && this.method != "") {
+      prefix = `EventEmitter#${this.method}`;
+    }
+    return `${prefix}: ${this.message}`;
+  }
 }
 
+/**
+ * 
+ * 
+ * @export
+ * @interface EventHandler
+ */
 export interface EventHandler {
   (...args: any[])
 }
 
+/**
+ * 
+ * 
+ * @export
+ * @interface Events
+ */
 export interface Events {
+  /**
+   * 
+   * 
+   * @type {string}
+   * @memberOf Events
+   */
   name: string
+  /**
+   * 
+   * 
+   * @type {boolean}
+   * @memberOf Events
+   */
   once: boolean
+  /**
+   * 
+   * 
+   * @type {EventHandler}
+   * @memberOf Events
+   */
   handler: EventHandler
+  /**
+   * 
+   * 
+   * @type {*}
+   * @memberOf Events
+   */
   ctx?: any
 }
 
+/**
+ * 
+ * 
+ * @export
+ * @interface IEventEmitter
+ */
 export interface IEventEmitter {
-  listeners: {[key: string]: Events[]}
+  /**
+   * 
+   * 
+   * @type {{ [key: string]: Events[] }}
+   * @memberOf IEventEmitter
+   */
+  listeners?: { [key: string]: Events[] }
+  /**
+   * 
+   * 
+   * @type {string}
+   * @memberOf IEventEmitter
+   */
   listenId?: string
-  on (event: string, fn:EventHandler, ctx?:any): any
-  once(event: string, fn:EventHandler, ctx?:any): any
-  off (event: string, fn?:EventHandler, ctx?:any): any
-  trigger (event: string, ...args:any[]): any
+  /**
+   * 
+   * 
+   * @param {string} event
+   * @param {EventHandler} fn
+   * @param {*} [ctx]
+   * @returns {*}
+   * 
+   * @memberOf IEventEmitter
+   */
+  on(event: string, fn: EventHandler, ctx?: any): any
+  /**
+   * 
+   * 
+   * @param {string} event
+   * @param {EventHandler} fn
+   * @param {*} [ctx]
+   * @returns {*}
+   * 
+   * @memberOf IEventEmitter
+   */
+  once(event: string, fn: EventHandler, ctx?: any): any
+  /**
+   * 
+   * 
+   * @param {string} event
+   * @param {EventHandler} [fn]
+   * @param {*} [ctx]
+   * @returns {*}
+   * 
+   * @memberOf IEventEmitter
+   */
+  off(event: string, fn?: EventHandler, ctx?: any): any
+  /**
+   * 
+   * 
+   * @param {string} event
+   * @param {...any[]} args
+   * @returns {*}
+   * 
+   * @memberOf IEventEmitter
+   */
+  trigger(event: string, ...args: any[]): any
 }
 
+/**
+ * 
+ * 
+ * @export
+ * @interface Destroyable
+ */
 export interface Destroyable {
-  destroy()
+  /**
+   * 
+   * 
+   * 
+   * @memberOf Destroyable
+   */
+  destroy();
 }
 
-export function callFunc (fn:Events[], args:any[] = []) {
+/**
+ * 
+ * 
+ * @export
+ * @param {Events[]} fn
+ * @param {any[]} [args=[]]
+ * @returns
+ */
+export function callFunc(fn: Events[], args: any[] = []) {
   let l = fn.length, i = -1, a1 = args[0], a2 = args[1],
     a3 = args[2], a4 = args[3];
 
-   switch (args.length) {
-     case 0: while (++i < l) fn[i].handler.call(fn[i].ctx); return;
-     case 1: while (++i < l) fn[i].handler.call(fn[i].ctx, a1); return;
-     case 2: while (++i < l) fn[i].handler.call(fn[i].ctx, a1, a2); return;
-     case 3: while (++i < l) fn[i].handler.call(fn[i].ctx, a1, a2, a3); return;
-     case 4: while (++i < l) fn[i].handler.call(fn[i].ctx, a1, a2, a3, a4); return;
-     default: while (++i < l) fn[i].handler.apply(fn[i].ctx, args); return;
-   }
+  switch (args.length) {
+    case 0: while (++i < l) fn[i].handler.call(fn[i].ctx); return;
+    case 1: while (++i < l) fn[i].handler.call(fn[i].ctx, a1); return;
+    case 2: while (++i < l) fn[i].handler.call(fn[i].ctx, a1, a2); return;
+    case 3: while (++i < l) fn[i].handler.call(fn[i].ctx, a1, a2, a3); return;
+    case 4: while (++i < l) fn[i].handler.call(fn[i].ctx, a1, a2, a3, a4); return;
+    default: while (++i < l) fn[i].handler.apply(fn[i].ctx, args); return;
+  }
 }
 
-export function isFunction (a:any): a is Function {
+/**
+ * 
+ * 
+ * @export
+ * @param {*} a
+ * @returns {a is Function}
+ */
+export function isFunction(a: any): a is Function {
   return typeof a === 'function';
 }
 
-export function isEventEmitter(a:any): a is EventEmitter {
+/**
+ * 
+ * 
+ * @export
+ * @param {*} a
+ * @returns {a is EventEmitter}
+ */
+export function isEventEmitter(a: any): a is EventEmitter {
   return a && (a instanceof EventEmitter || (isFunction(a.on) && isFunction(a.once) && isFunction(a.off) && isFunction(a.trigger)));
 }
 
 
+/**
+ * 
+ * 
+ * @export
+ * @class EventEmitter
+ * @implements {IEventEmitter}
+ * @implements {Destroyable}
+ */
 export class EventEmitter implements IEventEmitter, Destroyable {
+  /**
+   * 
+   * 
+   * @static
+   * @type {boolean}
+   * @memberOf EventEmitter
+   */
   static throwOnError: boolean = true;
+  /**
+   * 
+   * 
+   * @static
+   * 
+   * @memberOf EventEmitter
+   */
   static debugCallback: (className: string, name: string, event: string, args: any[], listeners: Events[]) => void
-  static executeListenerFunction: (func:Events[], args?: any[]) => void = function (func, args) {
-  	callFunc(func, args);
+  /**
+   * 
+   * 
+   * @static
+   * 
+   * @memberOf EventEmitter
+   */
+  static executeListenerFunction: (func: Events[], args?: any[]) => void = function (func, args) {
+    callFunc(func, args);
   }
 
+  /**
+   * 
+   * 
+   * @type {string}
+   * @memberOf EventEmitter
+   */
   listenId: string
   private _listeners: { [key: string]: Events[] }
   private _listeningTo: { [key: string]: any }
 
-  public get listeners (): {[key: string]: Events[]} {
+  /**
+   * 
+   * 
+   * @readonly
+   * @type {{ [key: string]: Events[] }}
+   * @memberOf EventEmitter
+   */
+  public get listeners(): { [key: string]: Events[] } {
     return this._listeners;
   }
 
-  on (event: string, fn:EventHandler, ctx?:any, once:boolean = false): any {
-    let events = (this._listeners|| (this._listeners = {}))[event]||(this._listeners[event]=[]);
+  /**
+   * 
+   * 
+   * @param {string} event
+   * @param {EventHandler} fn
+   * @param {*} [ctx]
+   * @param {boolean} [once=false]
+   * @returns {*}
+   * 
+   * @memberOf EventEmitter
+   */
+  on(event: string, fn: EventHandler, ctx?: any, once: boolean = false): any {
+    let events = (this._listeners || (this._listeners = {}))[event] || (this._listeners[event] = []);
 
     events.push({
       name: event,
       once: once,
       handler: fn,
-      ctx: ctx||this
+      ctx: ctx || this
     });
 
     return this
   }
 
-  once (event: string, fn:EventHandler, ctx?:any): any {
+  /**
+   * 
+   * 
+   * @param {string} event
+   * @param {EventHandler} fn
+   * @param {*} [ctx]
+   * @returns {*}
+   * 
+   * @memberOf EventEmitter
+   */
+  once(event: string, fn: EventHandler, ctx?: any): any {
     return this.on(event, fn, ctx, true);
   }
 
-  off (eventName?: string, fn?:EventHandler): any {
-      this._listeners = this._listeners || {};
-    if (eventName == null) {
+  /**
+   * 
+   * 
+   * @param {string} [eventName]
+   * @param {EventHandler} [fn]
+   * @param {*} [ctx]
+   * @returns {*}
+   * 
+   * @memberOf EventEmitter
+   */
+  off(eventName?: string, fn?: EventHandler, ctx?: any): any {
+    this._listeners = this._listeners || {};
+    if (eventName == null && ctx == null) {
       this._listeners = {};
-    } else if (this._listeners[eventName]){
+    } else if (this._listeners[eventName]) {
       let events = this._listeners[eventName];
-      if (fn == null) {
+      if (fn == null && ctx == null) {
         this._listeners[eventName] = [];
       } else {
-        for (let i=0;i<events.length;i++) {
-          let event = events[i];
-          if (events[i].handler == fn) {
-            this._listeners[eventName].splice(i,1);
-          }
+        for (let i = 0; i < events.length; i++) {
+          let e = events[i];
+          if ((fn == null && ctx != null && e.ctx === ctx) ||
+            (fn != null && ctx == null && e.handler === fn) ||
+            (fn != null && ctx != null && e.handler === fn && e.ctx === ctx))
+            //if (events[i].handler == fn) {
+            this._listeners[eventName].splice(i, 1);
+          //}
         }
       }
 
@@ -119,8 +346,17 @@ export class EventEmitter implements IEventEmitter, Destroyable {
     return this;
   }
 
-  trigger (eventName: string, ...args:any[]): any {
-    
+  /**
+   * 
+   * 
+   * @param {string} eventName
+   * @param {...any[]} args
+   * @returns {*}
+   * 
+   * @memberOf EventEmitter
+   */
+  trigger(eventName: string, ...args: any[]): any {
+
     this._listeners = this._listeners || {};
     let events = (this._listeners[eventName] || []).concat(this._listeners['all'] || []).concat(this._listeners["*"] || []);
 
@@ -128,11 +364,11 @@ export class EventEmitter implements IEventEmitter, Destroyable {
     if (EventEmitter.debugCallback)
       EventEmitter.debugCallback((<any>this.constructor).name, (<any>this).name, eventName, args, events)
 
-    let event, a, len = events.length, index;
+    let event, a, index;
     let calls: Events[] = [];
     let alls: Events[] = [];
 
-    for (let i=0, ii = events.length;i<ii;i++) {
+    for (let i = 0, ii = events.length; i < ii; i++) {
       event = events[i]
       a = args
 
@@ -144,13 +380,13 @@ export class EventEmitter implements IEventEmitter, Destroyable {
 
       if (events[i].once === true) {
         index = this._listeners[events[i].name].indexOf(events[i])
-        this._listeners[events[i].name].splice(index,1)
+        this._listeners[events[i].name].splice(index, 1)
       }
     }
 
     if (alls.length) {
-    	let a = [eventName].concat(args);
-    	this._executeListener(alls, a);
+      let a = [eventName].concat(args);
+      this._executeListener(alls, a);
     }
 
     if (calls.length) this._executeListener(calls, args);
@@ -159,55 +395,102 @@ export class EventEmitter implements IEventEmitter, Destroyable {
 
   }
 
+  /**
+   * 
+   * 
+   * @param {Events[]} func
+   * @param {any[]} [args]
+   * 
+   * @memberOf EventEmitter
+   */
   _executeListener(func: Events[], args?: any[]) {
-      EventEmitter.executeListenerFunction(func, args);
+    EventEmitter.executeListenerFunction(func, args);
   }
 
-  listenTo (obj: IEventEmitter, event: string, fn:EventHandler, ctx?:any, once:boolean = false): any {
-  	  if (!isEventEmitter(obj)) {
-  	  	if (EventEmitter.throwOnError)
-  	  		throw new EventEmitterError("obj is not an EventEmitter", once ? "listenToOnce" : "listenTo", this, obj);
-  	  	return this;
-  	  }
-
-      let listeningTo, id, meth;
-      listeningTo = this._listeningTo|| (this._listeningTo = {});
-      id = obj.listenId || (obj.listenId = getID())
-      listeningTo[id] = obj;
-      meth = once ? 'once' : 'on';
-
-      obj[meth](event, fn, this);
-
+  /**
+   * 
+   * 
+   * @param {IEventEmitter} obj
+   * @param {string} event
+   * @param {EventHandler} fn
+   * @param {*} [ctx]
+   * @param {boolean} [once=false]
+   * @returns {*}
+   * 
+   * @memberOf EventEmitter
+   */
+  listenTo(obj: IEventEmitter, event: string, fn: EventHandler, ctx?: any, once: boolean = false): any {
+    if (!isEventEmitter(obj)) {
+      if (EventEmitter.throwOnError)
+        throw new EventEmitterError("obj is not an EventEmitter", once ? "listenToOnce" : "listenTo", this, obj);
       return this;
+    }
+
+    let listeningTo, id, meth;
+    listeningTo = this._listeningTo || (this._listeningTo = {});
+    id = obj.listenId || (obj.listenId = getID())
+    listeningTo[id] = obj;
+    meth = once ? 'once' : 'on';
+
+    obj[meth](event, fn, this);
+
+    return this;
   }
 
-  listenToOnce (obj: IEventEmitter, event: string, fn:EventHandler, ctx?:any): any {
+  /**
+   * 
+   * 
+   * @param {IEventEmitter} obj
+   * @param {string} event
+   * @param {EventHandler} fn
+   * @param {*} [ctx]
+   * @returns {*}
+   * 
+   * @memberOf EventEmitter
+   */
+  listenToOnce(obj: IEventEmitter, event: string, fn: EventHandler, ctx?: any): any {
     return this.listenTo(obj, event, fn, ctx, true)
   }
 
-  stopListening (obj?: IEventEmitter, event?:string, callback?:EventHandler) {
-  	  if (obj && !isEventEmitter(obj)) {
-  	  	if (EventEmitter.throwOnError)
-  	  		throw new EventEmitterError("obj is not an EventEmitter", "stopListening", this, obj);
-  	  	return this;
-  	  }
-
-      let listeningTo: any = this._listeningTo;
-      if (!listeningTo) return this;
-      var remove = !event && !callback;
-      if (!callback && typeof event === 'object') callback = <any>this;
-      if (obj) (listeningTo = {})[obj.listenId] = obj;
-
-      for (var id in listeningTo) {
-        obj = listeningTo[id];
-        obj.off(event, callback, this);
-
-        if (remove || !Object.keys(obj.listeners).length) delete this._listeningTo[id];
-      }
+  /**
+   * 
+   * 
+   * @param {IEventEmitter} [obj]
+   * @param {string} [event]
+   * @param {EventHandler} [callback]
+   * @returns
+   * 
+   * @memberOf EventEmitter
+   */
+  stopListening(obj?: IEventEmitter, event?: string, callback?: EventHandler) {
+    if (obj && !isEventEmitter(obj)) {
+      if (EventEmitter.throwOnError)
+        throw new EventEmitterError("obj is not an EventEmitter", "stopListening", this, obj);
       return this;
+    }
+
+    let listeningTo: any = this._listeningTo;
+    if (!listeningTo) return this;
+    var remove = !event && !callback;
+    if (!callback && typeof event === 'object') callback = <any>this;
+    if (obj) (listeningTo = {})[obj.listenId] = obj;
+
+    for (var id in listeningTo) {
+      obj = listeningTo[id];
+      obj.off(event, callback, this);
+
+      if (remove || !Object.keys(obj.listeners).length) delete this._listeningTo[id];
+    }
+    return this;
   }
 
-  destroy () {
+  /**
+   * 
+   * 
+   * 
+   * @memberOf EventEmitter
+   */
+  destroy() {
     this.stopListening();
     this.off();
   }
